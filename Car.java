04e057e096561beb5c4ad2224d38c329cc85a40c
sarahjanewhite <45084719+sarahjanewhite@users.noreply.github.com>
2023-10-19 00:59:14 +0000

    Sarah Skeleton

--- Car.java
+++ Car.java
@@ -21,8 +21,29 @@ public class Car {
 
     // Accessor methods
     // Accessor methods to retrieve the values of the member variables.
+    public int getMpg() {
+        // Assigned to Sarah
 
+    }
+
+    public double getCost() {
+        // Assigned to Sarah
+
+    }
+
+    // Setter methods
     // Setter methods to modify the values of the member variables.
+    public void setMpg(int mpg) {
+        // Assigned to Sarah
+        setMpg = mpg;
+
+    }
+
+    public void setCost(double cost) {
+        // Assigned to Sarah
+        setCost = cost;
+
+    }
 
     // Method to mark the car as sold and calculate the profit
     // Method for selling the car and calculating profit.
@@ -30,6 +51,11 @@ public class Car {
     // Comparison methods
     // Methods for comparing Car objects based on specific attributes (mpg, mileage,
     // price).
+    public int compareMPG(Car otherCar) {
+        // Assigned to Sarah
+
+
+    }
 
     // toString method
     @Override

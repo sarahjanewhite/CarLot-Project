eee72841894834a243d9de0cc8ea79f247320e85
zok! <112133076+Zokibi@users.noreply.github.com>
2023-10-19 16:39:55 -0400

    Rearranged + fixed methods, added basic javadoc.

--- Car.java
+++ Car.java
@@ -11,26 +11,36 @@ public class Car {
 
     // Default Constructor
     public Car() {
-        // Default constructor initializes a Car object with default values.
-        this.setId("default1");
-        this.setMileage(0);
-        this.setMpg(0);
-        this.setCost(0.0);
-        this.setSalesPrice(0.0);
-        this.setSold(false);
-        this.setPriceSold(0.0);
-        this.setProfit(0.0);
+        /**
+         * Default constructor initializes a Car object with default values.
+         *
+         **/
+        this("default1", 0, 0, 0.0, 0.0);
     }
 
-    // Constructor for adding a new Car to the inventory
+    /**
+     * Constructor used when creating a new Car that will be added to a CarLot.
+     *
+     * @param id         String representation to identify this car. Should not
+     *                   contain spaces.
+     * @param mileage    Mileage of the vehicle when added to inventory.
+     * @param mpg        Miles Per Gallon.
+     * @param cost       Price paid to acquire this car.
+     * @param salesPrice Price that the CarLot wants to sell this car for. Note that
+     *                   when a vehicle is sold,
+     *                   the price that it sells for may be different than the sales
+     *                   price.
+     */
     public Car(String id, int mileage, int mpg, double cost, double salesPrice) {
         // Constructor for creating a new Car object with specified attributes.
-        this.setId(id);
-        this.setMileage(mileage);
-        this.setMpg(mpg);
-        this.setCost(cost);
-        this.setSalesPrice(salesPrice);
-
+        this.id = id;
+        this.mileage = mileage;
+        this.mpg = mpg;
+        this.cost = cost;
+        this.salesPrice = salesPrice;
+        this.sold = false;
+        this.priceSold = 0.0;
+        this.profit = 0.0;
     }
 
     // Accessor methods
@@ -45,112 +55,77 @@ public class Car {
 
     }
 
-    // Setter methods
-    // Setter methods to modify the values of the member variables.
-    public void setMpg(int mpg) {
-        setMpg = mpg;
+    // getId
 
+    // getMileage
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public boolean isSold() {
+        return sold;
+    }
+
+    public double getPriceSold() {
+        return priceSold;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    // Setter methods
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
     }
 
     public void setCost(double cost) {
-        setCost = cost;
-
+        this.cost = cost;
     }
 
-    // Method to mark the car as sold and calculate the profit
-    // Method for selling the car and calculating profit.
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // setMileage
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public void setSold(boolean sold) {
+        this.sold = sold;
+    }
+
+    public void setPriceSold(double priceSold) {
+        this.priceSold = priceSold;
+        calculateProfit();
+    }
+
+    // Method to mark the car as sold and calculate profit
+    public void sellCar(double priceSold) {
+        this.sold = true;
+        this.setPriceSold(priceSold);
+    }
 
     // Comparison methods
-    // Methods for comparing Car objects based on specific attributes (mpg, mileage,
-    // price).
-    public int compareMPG(Car otherCar) {
-        if (mpg < otherCar.mpg) {
-            return -1;
-        } else if (mpg > otherCar.mpg) {
-            return 1;
-        } else {
-            return 0;
-        }
-
-
+    public int compareMPG(Car otheCar) {
+        return Double.compare(this.mileage, otheCar.mileage);
     }
 
-    // toString method
+    // compareMiles
+
+    private void calculateProfit() {
+        profit = priceSold - cost;
+    }
+
     @Override
     public String toString() {
-        // Method to generate a formatted string representation of the Car object.
-    }
-
-    /**
-    * Displays a text representation of a specific Car object 
-    */
-    
-    public String toString() {
-    
-        System.out.println("Car ID: " + getId());
-        System.out.println("Mileage: " +getMileage());
-        System.out.println("MPG: " +getMpg());
-        System.out.println("Cost: $" + getCost());
-        System.out.println("Sales Price: $" +getSalesPrice());
-        System.out.println("Sold?: " +isSold());
-        System.out.println("Price Sold: $" +getPriceSold());
-        System.out.println("Profit: $" +getProfit());
-    }
-     
-    /**
-    * Compares the sales price of this instance of car to another instance
-    * Returns a negative number if this car's sales price is lower
-    * Returns zero if the prices are equal
-    * Returns a positive number is this car's sales price is higher
-    *
-    * @param Car otherCar This is the car whose sales price will be compared
-    */
-    
-    public double compareSalesprice(Car otherCar) {
-    
-        double difference = getPriceSold() - otherCar.getPriceSold();
-    
-        return difference;
-    }
-    
-    /**
-    * Returns the asking price of the current car object
-    */
-    
-    public double getSalesPrice() {
-        
-        return salesPrice;
-    }
-    
-    /**
-    * Allows the user to change the asking price of the current car object
-    * Returns the new asking price
-    *
-    * @param double salesPrice The asking price for the car
-    */
-    public double setSalesPrice (double salesPrice) {
-        this.salesPrice = salesPrice ;
-        return this.salesPrice;
-    }
-    
-    /**
-    * Returns the actual sale price of the current car object
-    */
-    
-    public double getPriceSold() {
-        
-        return priceSold;
-    }
-    
-    /**
-    * Allows the user to enter the price the car was sold for
-    * Returns the new sales price
-    *
-    * @param double priceSold The price the car was sold for
-    */
-    
-    public double setPriceSold (double priceSold) {
-        this.priceSold = priceSold;
-        return this.priceSold;
+        return "Car: " + id + ", Mileage: " + mileage + ", MPG " + mpg +
+                ", Sold: " + (sold ? "Yes" : "No") + ", Cost: $" + String.format("%.2f", cost) +
+                ", Selling price: $" + String.format("%.2f", salesPrice) +
+                ", Sold For $" + String.format("%.2f", priceSold) +
+                ", Profit: $" + String.format("%.2f", profit);
     }
 }

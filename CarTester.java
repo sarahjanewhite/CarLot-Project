f7e3fd2d591c7e9680a559acb8dcb10f7cd3f7fa
sarahjanewhite <sjwhite1@my.waketech.edu>
2023-10-19 20:16:52 -0400

    Update CarTester.java
    Created a basic tester program. Things will need to be added when the Car class is complete.

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,51 @@
+import java.util.Scanner;
 
+public class CarTester {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create a new Car using the default constructor
+        Car defaultCar = new Car();
+        System.out.println("\nDefault Car:\n" + defaultCar + "\n");
+
+        // Get user input to create a new Car
+        System.out.println("Enter New User's Car details:");
+        System.out.print("ID: ");
+        String id = scanner.nextLine();
+
+        System.out.print("Mileage: ");
+        int mileage = scanner.nextInt();
+
+        System.out.print("MPG: ");
+        int mpg = scanner.nextInt();
+
+        System.out.print("Cost: ");
+        double cost = scanner.nextDouble();
+
+        System.out.print("Sales Price: ");
+        double salesPrice = scanner.nextDouble();
+
+        // Creates and prints a new Car with user input
+        Car userCar = new Car(id, mileage, mpg, cost, salesPrice);
+        System.out.println("\nUser's Car:\n" + userCar);
+
+        // Ask the user if they want to sell the car
+        System.out.print("\nDo you want to sell the car? (yes/no): ");
+        scanner.nextLine();
+        String sellChoice = scanner.nextLine().toLowerCase();
+
+        if (sellChoice.equals("yes")) {
+            System.out.print("Enter the selling price: ");
+            double sellingPrice = scanner.nextDouble();
+
+            // Sell the car print statements
+            userCar.sellCar(sellingPrice);
+            System.out.println("\nSold User's Car:\n" + userCar);
+        } else {
+            System.out.println("\nUser chose not to sell the car.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}

a417c1c7818e1db8ef3772276f9b7e330a930a29
k-alexander-lab <70921316+k-alexander-lab@users.noreply.github.com>
2023-10-23 22:12:18 -0400

    Create CarLot.java
    Added file and skeleton for CarLot class

---
+++ CarLot.java
@@ -0,0 +1,70 @@
+import java.util.*;
+//Do we need an import statement for Car class?
+
+public class CarLot {
+   ArrayList<Car> inventory = new ArrayList<Car>();
+
+   public void addCar​(String id, int mileage, int mpg, double cost, double salesPrice) {
+     
+   }
+
+   public CarLot() {
+     ArrayList<Car> = //something;
+   }
+   
+  public Car findCarByIdentifier​(String identifier) {
+     return
+   }
+
+  public double getAverageMpg() {
+    return
+  }
+
+  public ArrayList<Car> getCarsSortedByMPG() {
+    return
+  }
+
+  public ArrayList<Car> getCarsInOrderOfEntry() {
+    return
+  }
+
+  public Car getCarWithBestMPG() {
+    return 
+  }
+
+  public Car getCarWithHighestMileage() {
+    return
+  }
+
+  ArrayList<Car> 	getInventory() {
+    return
+  }
+
+  public double getTotalProfit() {
+    return
+  }
+
+  public void sellCar​(java.lang.String identifier, double priceSold) {
+    
+  }
+
+  ///////***** Not listed for this week's assignment in Blackboard but included in JavaDoc *****/////////
+
+  public void setInventory​(ArrayList<Car> inventory)  {
+    
+  }
+
+  public void loadFromDisk() {
+    
+  }
+
+  public void saveToDisk() {
+    
+  }
+  
+  
+  
+}
+
+                  
+

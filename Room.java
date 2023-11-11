a44eaf53220d21c8263d1a601fa164796d636d70
Aliyah <aliyah.y.lewis@gmail.com>
2023-11-11 13:46:38 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,42 +1,52 @@
-import java.util.*;
-
 /**
  * Room
  * 
  * A class to simulate a room
  *
- * @author 
+ * @author Jacob Sparks, Aliyah Lewis
  *
  */
 
 
 public class Room {
-   private int squareFeet;
+   private int area;
+
+   /**
+     * Constructs a room with a specific area.
+     *
+     * @param area | The area of the room in square feet.
+     */
+   public Room(int area){
+    this.area = area;
+   }
 
     /**
      * Accessor that retrieves the area of the room
      * in square feet
-     * @return the area of the room in square feet
+     * 
+     * @return area | The area of the room in square feet
      */
     
      public int getSquareFeet() {
-     return squareFeet;
+     return area;
    }
 
    /**
     * Mutator method that sets the area of the room
     * in square feet
-    * @param int area, the area of the room in square feet
+    *
+    * @param int area | The area of the room in square feet
     */
 
    public void setSquareFeet(int area) {
-     this.squareFeet = area;
+     this.area = area;
    }
 
     /**
     * Accessor method that retrieves the capacity of the room.
     * The capacity is how many chairs that can fit in the room.
-    * @return the capacity of the room
+    *
+    * @return capacity | the capacity of the room
     */
 
    public int getCapacity() {
@@ -44,23 +54,21 @@ public class Room {
     //The capacity is given by dividing the square feet 
     //by 9 (using integer division).
 
+    int capacity = area / 9;
+
     return capacity;
    }
 
     /**
-    * Construct a Room
-    */
-
-   public Room(int area) {
-    squareFeet = area;
-  }
-
-    /**
     * Display information about the room
+    *
+    * return String | A string displaying the area and capacity of the room
     */
 
     public String toString() {
         // returns the square feet and capacity of the room.
+        String String = "Square Feet: " + area + ", Capacity: " + getCapacity();
+
         return String; 
     }
 

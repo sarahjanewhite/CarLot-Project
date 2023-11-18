8922560b44a68a0ee110ca31626a7ee49636a2d2
k-alexander-lab <70921316+k-alexander-lab@users.noreply.github.com>
2023-11-18 14:45:58 -0500

    Update CarLot.java

--- CarLot.java
+++ CarLot.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
 import javax.management.RuntimeErrorException;
+//please work
 
 public class CarLot extends ArrayList<Car> {
 

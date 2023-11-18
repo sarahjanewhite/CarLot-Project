ba6cdb7facb0be2b2baa8870e1f70ffa62913c75
sarahjanewhite <sjwhite1@my.waketech.edu>
2023-11-18 14:55:51 -0500

    Update CarLot.java
    Commenting out a utility

--- CarLot.java
+++ CarLot.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Collections;
+// import java.util.Collections;
 // import javax.management.RuntimeErrorException;
 
 public class CarLot extends ArrayList<Car> {
